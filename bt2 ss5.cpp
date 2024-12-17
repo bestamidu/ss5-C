02c22af482ed48d9fab6d31f044633b01221ad02
bestamidu <ynhu22887@gmail.com>
2024-12-17 22:34:48 +0700

    2

---
+++ bt2 ss5.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n = 20; 
+    int i;      
+
+    printf("Nhap mot so: ");
+    scanf("%d", &i);
+
+    while (i == n) { 
+        printf("dung r\n: ");
+    }
+
+    printf("So k trung khop roi!\n");
+
+    return 0;
+}
+
